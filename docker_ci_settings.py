9d313afc37572882b2b356c73a3be5b2491f3e18
dalred <RcrF63gGG7HpbAT>
2022-05-31 16:44:30 +0300

    add auth

--- docker_ci_settings.py
+++ docker_ci_settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'rest_framework',
     'users',
     'drf_spectacular',
