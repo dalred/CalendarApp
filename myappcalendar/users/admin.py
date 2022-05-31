4ab4362b01e37a98fcb2cc1dc092366207478cce
dalred <RcrF63gGG7HpbAT>
2022-05-31 09:55:34 +0300

    Final

--- myappcalendar/users/admin.py
+++ myappcalendar/users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-
+from django.db.models import Count
 user = get_user_model()
 
 
 class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ("last_login",)
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ['email', 'first_name', 'last_name']
     # TODO
