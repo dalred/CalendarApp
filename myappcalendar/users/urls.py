806bef8aa74a04f5f9f13be62b7cca9184f01100
dalred <RcrF63gGG7HpbAT>
2022-05-31 16:33:02 +0300

    add front

--- myappcalendar/users/urls.py
+++ myappcalendar/users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('users/', users.views.UserListAPIView.as_view()),
     path('users/<int:pk>/', users.views.UserRetrieveView.as_view()),
     path('users/create/', users.views.UserCreate.as_view()),
-    path('login', views.obtain_auth_token),
+    path('login/', views.obtain_auth_token),
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view())
 ]
