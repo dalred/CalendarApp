d0c2d4a7c7afdff8e3c03db76f67d15600825de9
dalred <RcrF63gGG7HpbAT>
2022-06-26 02:28:41 +0300

    add changes to admin

--- myappcalendar/goals/admin.py
+++ myappcalendar/goals/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from goals.models import Goal, GoalCategory, GoalComment
+from goals.models import Goal, GoalCategory, GoalComment, Board, BoardParticipant
+
 
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ("title", "user", "created", "updated")
     search_fields = ("title", "user")
+
 
 class GoalAdmin(admin.ModelAdmin):
     list_display = ("title", "user", "created", "updated", "status")
@@ -17,6 +19,20 @@ class GoalCommentAdmin(admin.ModelAdmin):
     list_filter = ('goal',)
 
 
+class BoardParticipantAdmin(admin.ModelAdmin):
+    list_display = ("board", "user", "role")
+    search_fields = ("board",)
+    list_filter = ('board',)
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_deleted")
+    search_fields = ("title",)
+    list_filter = ('is_deleted',)
+
+
 admin.site.register(GoalCategory, GoalCategoryAdmin)
 admin.site.register(Goal, GoalAdmin)
 admin.site.register(GoalComment, GoalCommentAdmin)
+admin.site.register(BoardParticipant, BoardParticipantAdmin)
+admin.site.register(Board, BoardAdmin)
