34629841c072891450f0ae6654ebc8d1591fe604
dalred <RcrF63gGG7HpbAT>
2022-07-09 18:22:20 +0300

    final

--- myappcalendar/myappcalendar/settings.py
+++ myappcalendar/myappcalendar/settings.py
@@ -103,6 +103,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 3,  # при лист выдает ограниченное количество значений. Limit будет ингнорировать PageSize
+    # так же влияет на results[]
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     # 'DEFAULT_FILTER_BACKENDS': (
