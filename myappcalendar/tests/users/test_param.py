f1e70a305dadf0914eaf6f57c134ed03d1e1dd74
dalred <RcrF63gGG7HpbAT>
2022-08-05 16:52:13 +0300

    add test param

---
+++ myappcalendar/tests/users/test_param.py
@@ -0,0 +1,11 @@
+import pytest
+import testit
+
+@testit.title('Тест параметризованный')
+@testit.description('Автотесты сущности пользователь')
+@testit.displayName('Автотест параметризованный')
+@testit.externalID('Тестовый параметризованный тест')
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("2+2", 4)])
+def test_param_testit(test_input, expected):
+    with testit.step('Первый шаг'):
+        assert eval(test_input) == expected
