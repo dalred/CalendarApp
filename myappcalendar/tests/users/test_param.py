b45f514ed3645fab22c0a8479814597800335d59
dalred <RcrF63gGG7HpbAT>
2022-08-16 15:47:25 +0300

    add changes to tests

--- myappcalendar/tests/users/test_param.py
+++ myappcalendar/tests/users/test_param.py
@@ -6,7 +6,7 @@ import testit
 @testit.description('Автотесты сущности пользователь')
 @testit.displayName('Автотест параметризованный')
 @testit.externalID('Тестовый параметризованный тест')
-@pytest.mark.parametrize("login, password, expected_login_password", [("testit@testit.ru", "124", "testit@testit.ru_123")])
+@pytest.mark.parametrize("login, password, expected_login_password", [("testit@testit.ru", "123", "testit@testit.ru_123")])
 def test_param_testit(login, password, expected_login_password):
     with testit.step('Первый шаг'):
         assert str(f"{login}_{password}") == expected_login_password
