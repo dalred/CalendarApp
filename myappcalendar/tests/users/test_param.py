341818de67cd3cda44fed332ba1008a80599c467
dalred <RcrF63gGG7HpbAT>
2022-08-24 12:42:40 +0300

    add changes to tests

--- myappcalendar/tests/users/test_param.py
+++ myappcalendar/tests/users/test_param.py
@@ -1,10 +1,10 @@
 import pytest
 import testit
 
-
-@testit.title('Тест параметризованный')
+@testit.workItemID(18)
+@testit.title('Тест параметризованный one')
 @testit.description('Автотесты сущности пользователь')
-@testit.displayName('Автотест параметризованный')
+@testit.displayName('Автотест параметризованный one')
 @testit.externalID('Тестовый параметризованный тест')
 @pytest.mark.parametrize("login, password, expected_login_password", [("testit@testit.ru", "123", "testit@testit.ru_123"), ("testit@testit.ru", "124", "testit@testit.ru_124")])
 def test_param_testit(login, password, expected_login_password):
@@ -18,5 +18,15 @@ def test_param_testit(login, password, expected_login_password):
 @testit.externalID('Тестовый параметризованный тест2')
 @pytest.mark.parametrize("login, password, expected_login_password", [("testit@testit.ru", "123", "testit@testit.ru_123")])
 def test_param2_testit(login, password, expected_login_password):
+    with testit.step('Первый шаг'):
+        assert str(f"{login}_{password}") == expected_login_password
+
+@testit.title('Тест параметризованный')
+@testit.description('Автотесты сущности пользователь')
+@testit.displayName('Автотест параметризованный')
+@testit.externalID('Тестовый параметризованный тест3')
+@pytest.mark.xfail(reason="bug in a 3rd party library")
+@pytest.mark.parametrize("login, password, expected_login_password", [("testit@testit.ru", "123", "testit@testit.ru_123")])
+def test_param3_testit(login, password, expected_login_password):
     with testit.step('Первый шаг'):
         assert str(f"{login}_{password}") == expected_login_password
