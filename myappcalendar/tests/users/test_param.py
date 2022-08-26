e1869ed5de47102d9cab46908349d3dadb9841d3
dalred <RcrF63gGG7HpbAT>
2022-08-26 12:05:34 +0300

    add changes to tests

--- myappcalendar/tests/users/test_param.py
+++ myappcalendar/tests/users/test_param.py
@@ -18,7 +18,7 @@ def test_param_testit(login, password, expected_login_password):
 @testit.displayName('Автотест параметризованный')
 @testit.externalID('Тестовый параметризованный тест2')
 #@pytest.mark.skip(reason="bug in a 3rd party library")
-@pytest.mark.parametrize("login, password, expected_login_password", [("testit@testit.ru", "123", "testit@testit.ru_123")])
+@pytest.mark.parametrize("login, password, expected_login_password", [("testit@testit.ru", "123", "testit@testit.ru_123"), ("testit2@testit.ru", "123", "testit2@testit.ru_123")])
 def test_param2_testit(login, password, expected_login_password):
     with testit.step('Первый шаг'):
         assert str(f"{login}_{password}") == expected_login_password
